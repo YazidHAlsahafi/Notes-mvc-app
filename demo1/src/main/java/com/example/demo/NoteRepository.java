a802cd3fe79b6ede1853e81ca4d7f2e2e4165e9e
AhmdAfif <h.s.ahmadafif@gmail.com>
2024-10-30 00:35:15 +0300

    delet demo

--- demo1/src/main/java/com/example/demo/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-// src/main/java/com/example/demo1/repository/NoteRepository.java
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
-
